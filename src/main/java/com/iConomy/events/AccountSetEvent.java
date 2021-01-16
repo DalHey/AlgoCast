1893e9f4f3c4ace2376f2952a23999ec0c3f5d8a
DalHey <97790586+DalHey@users.noreply.github.com>
2021-01-16 19:35:04 +0000

    test ssh

--- src/main/java/com/iConomy/events/AccountSetEvent.java
+++ src/main/java/com/iConomy/events/AccountSetEvent.java
@@ -5,4 +5,8 @@ import org.bukkit.event.HandlerList;
 
 public class AccountSetEvent extends Event {
 
-	private final Stri
+	private final String account;
+	private double balance;
+	private HandlerList handlers = new HandlerList();
+
+	public
