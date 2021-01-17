a66dc5497da5532322bb9fab97bfdc5f9f0fe54b
DalHey <97790586+DalHey@users.noreply.github.com>
2021-01-17 23:00:36 +0000

    0.7.13

--- src/main/java/com/iConomy/events/AccountUpdateEvent.java
+++ src/main/java/com/iConomy/events/AccountUpdateEvent.java
@@ -3,4 +3,9 @@ package com.iConomy.events;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
-public class AccountUpdateEvent extends Even
+public class AccountUpdateEvent extends Event {
+
+	private final String account;
+	private double balance;
+	private double previous;
+	privat
