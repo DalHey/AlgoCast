1e543f2109ec941ede95ef120d4836bb36e2d57a
DalHey <97790586+DalHey@users.noreply.github.com>
2021-06-23 05:56:28 +0000

    Changed a few things

--- src/main/java/com/iConomy/net/Database.java
+++ src/main/java/com/iConomy/net/Database.java
@@ -238,4 +238,8 @@ public class Database {
         if(ps != null)
             try { ps.close(); } catch (SQLException ex) { }
 
-        
+        if(rs != null)
+            try { rs.close(); } catch (SQLException ex) { }
+
+        if(conn != null)
+   
