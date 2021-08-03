fb7c71d84cd6e4eb6a2eaefea5e4603f49816652
DalHey <97790586+DalHey@users.noreply.github.com>
2021-08-03 07:23:24 +0000

    clean up project

--- src/main/java/com/iConomy/system/Account.java
+++ src/main/java/com/iConomy/system/Account.java
@@ -1,3 +1,7 @@
 package com.iConomy.system;
 
-import com.iConomy.events
+import com.iConomy.events.AccountRemoveEvent;
+import java.sql.ResultSet;
+
+import com.iConomy.iConomy;
+import com.iConomy
