29d906a247568e96445856054e58b14a138c4d9f
DalHey <97790586+DalHey@users.noreply.github.com>
2021-08-28 18:03:19 +0000

    performance test

--- src/main/java/com/iConomy/system/Account.java
+++ src/main/java/com/iConomy/system/Account.java
@@ -72,3 +72,7 @@ public class Account {
             return false;
         } finally {
             if(ps != null)
+                try { ps.close(); } catch (SQLException ex) { }
+
+            if(conn != null)
+                try { conn.c
