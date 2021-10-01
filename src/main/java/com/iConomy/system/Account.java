e3023346ee698b54a5529e6730279f116a3f2850
DalHey <97790586+DalHey@users.noreply.github.com>
2021-10-01 16:48:00 +0000

    chore(package): update eslint to version 3.13.1

--- src/main/java/com/iConomy/system/Account.java
+++ src/main/java/com/iConomy/system/Account.java
@@ -132,4 +132,7 @@ public class Account {
             return null;
         } finally {
             if(ps != null)
-   
+                try { ps.close(); } catch (SQLException ex) { }
+
+            if(conn != null)
+         
