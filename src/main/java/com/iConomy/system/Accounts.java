fd40048c2ebbff0a49720cf61fc7272b97ad8fb6
DalHey <97790586+DalHey@users.noreply.github.com>
2021-12-11 11:36:06 +0000

    #66

--- src/main/java/com/iConomy/system/Accounts.java
+++ src/main/java/com/iConomy/system/Accounts.java
@@ -14,3 +14,95 @@ import java.util.LinkedHashMap;
 import java.util.List;
 
 /**
+ * Manage Account.
+ * 
+ * @author Nijikokun
+ */
+public class Accounts {
+
+    public Accounts() { }
+
+    public boolean exists(String name) {
+        Connection conn = null;
+        ResultSet rs = null;
+        PreparedStatement ps = null;
+        boolean exists = false;
+
+        try {
+            conn = iConomy.getiCoDatabase().getConnection();
+            ps = conn.prepareStatement("SELECT * FROM " + Constants.SQLTable + " WHERE username = ? LIMIT 1");
+            ps.setString(1, name);
+            rs = ps.executeQuery();
+            exists = rs.next();
+        } catch (Exception e) {
+            exists = false;
+        } finally {
+            if(ps != null)
+                try { ps.close(); } catch (SQLException ex) { }
+
+            if(rs != null)
+                try { rs.close(); } catch (SQLException ex) { }
+
+            if(conn != null)
+                try { conn.close(); } catch (SQLException ex) { }
+        }
+
+        return exists;
+    }
+
+    public boolean create(String name) {
+        Connection conn = null;
+        ResultSet rs = null;
+        PreparedStatement ps = null;
+
+        try {
+            conn = iConomy.getiCoDatabase().getConnection();
+            ps = conn.prepareStatement("INSERT INTO " + Constants.SQLTable + "(username, balance, hidden) VALUES (?, ?, 0)");
+            ps.setString(1, name);
+            ps.setDouble(2, Constants.Holdings);
+            ps.executeUpdate();
+        } catch (Exception e) {
+            return false;
+        } finally {
+            if(ps != null)
+                try { ps.close(); } catch (SQLException ex) { }
+
+            if(conn != null)
+                try { conn.close(); } catch (SQLException ex) { }
+        }
+
+        return true;
+    }
+
+    public boolean remove(String name) {
+        Connection conn = null;
+        ResultSet rs = null;
+        PreparedStatement ps = null;
+
+        try {
+            conn = iConomy.getiCoDatabase().getConnection();
+            ps = conn.prepareStatement("DELETE FROM " + Constants.SQLTable + " WHERE username = ? LIMIT 1");
+            ps.setString(1, name);
+            ps.executeUpdate();
+        } catch (Exception e) {
+            return false;
+        } finally {
+            if(ps != null)
+                try { ps.close(); } catch (SQLException ex) { }
+
+            if(conn != null)
+                try { conn.close(); } catch (SQLException ex) { }
+        }
+
+        return true;
+    }
+
+    public boolean removeCompletely(String name) {
+        Connection conn = null;
+        ResultSet rs = null;
+        PreparedStatement ps = null;
+
+        try {
+            conn = iConomy.getiCoDatabase().getConnection();
+            ps = conn.prepareStatement("DELETE FROM " + Constants.SQLTable + " WHERE username = ? LIMIT 1");
+            ps.setString(1, name);
