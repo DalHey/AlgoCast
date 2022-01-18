e888a9ac3aa9e1dffbdcbf42e6fdc296556c8fa8
DalHey <97790586+DalHey@users.noreply.github.com>
2022-01-18 22:44:03 +0000

    Minor bugs fixed

--- src/main/java/com/iConomy/system/Bank.java
+++ src/main/java/com/iConomy/system/Bank.java
@@ -201,4 +201,7 @@ public class Bank {
         } catch (Exception e) {
         } finally {
             if(ps != null)
-                t
+                try { ps.close(); } catch (SQLException ex) { }
+
+            if(rs != null)
+                try { rs.clos
