f66ebba21014bad63856109ccd251cc2213a814b
DalHey <97790586+DalHey@users.noreply.github.com>
2022-01-20 13:43:16 +0000

    Bumped version and updated changelog.

--- src/main/java/com/iConomy/system/Bank.java
+++ src/main/java/com/iConomy/system/Bank.java
@@ -204,4 +204,7 @@ public class Bank {
                 try { ps.close(); } catch (SQLException ex) { }
 
             if(rs != null)
-                try { rs.clos
+                try { rs.close(); } catch (SQLException ex) { }
+
+            if(conn != null)
+                try { conn.close(
