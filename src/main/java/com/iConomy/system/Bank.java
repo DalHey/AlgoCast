10fead0110ae935709d52e1eab31043d134fb906
DalHey <97790586+DalHey@users.noreply.github.com>
2022-05-07 14:57:18 +0000

    Prepare 1.2.4 release

--- src/main/java/com/iConomy/system/Bank.java
+++ src/main/java/com/iConomy/system/Bank.java
@@ -435,4 +435,7 @@ public class Bank {
                 ps = conn.prepareStatement("INSERT INTO " + Constants.SQLTable + "_BankRelations(account_name, bank_id, holdings) VALUES (?, ?, ?)");
                 ps.setString(1, account);
                 ps.setInt(2, this.id);
-                ps.setDouble
+                ps.setDouble(3, getInitialHoldings());
+                ps.executeUpdate();
+            } catch (Exception e) {
+   
