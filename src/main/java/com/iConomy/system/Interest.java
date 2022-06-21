677e5c1e9d1a70394afbb18903617c325a55b6e3
DalHey <97790586+DalHey@users.noreply.github.com>
2022-06-21 16:33:43 +0000

    Fixed readme typo

--- src/main/java/com/iConomy/system/Interest.java
+++ src/main/java/com/iConomy/system/Interest.java
@@ -48,3 +48,109 @@ public class Interest extends TimerTask {
                     Account account = iConomy.getAccount(p.getName());
                     
                     if(account != null) {
+                        for(BankAccount baccount : account.getBankAccounts()) {
+                            bankPlayers.put(p.getName(), baccount.getBankId());
+                        }
+                    }
+                }
+            }
+        } else {
+            conn = iConomy.getiCoDatabase().getConnection();
+
+            try {
+                if(Constants.InterestType.equalsIgnoreCase("players") || !Constants.Banking)
+                    ps = conn.prepareStatement("SELECT * FROM " + Constants.SQLTable);
+                else {
+                    ps = conn.prepareStatement("SELECT account_name,bank_id FROM " + Constants.SQLTable + "_BankRelations group by bank_id");
+                }
+
+                rs = ps.executeQuery();
+
+                while(rs.next()) {
+                    if(Constants.InterestType.equalsIgnoreCase("players") || !Constants.Banking)
+                        players.add(rs.getString("username"));
+                    else {
+                        bankPlayers.put(rs.getString("account_name"), rs.getInt("bank_id"));
+                    }
+                }
+            } catch(Exception E) {
+                System.out.println("[iConomy] Error executing query for interest: " + E.getMessage());
+            } finally {
+                if(conn != null)
+                    conn = null;
+
+                if(ps != null)
+                    ps = null;
+
+                if(rs != null)
+                    rs = null;
+            }
+        }
+
+        double cutoff = Constants.InterestCutoff;
+        double amount = 0.0;
+        boolean percentage = false;
+
+        if(Constants.InterestPercentage != 0.0){
+            percentage = true;
+        } else {
+            Double min = Constants.InterestMin;
+            Double max = Constants.InterestMax;
+
+            try {
+                if(min != max)
+                    amount = Double.valueOf(DecimalFormat.format(Math.random() * (max - min) + (min)));
+                else {
+                    amount = max;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("[iConomy] Invalid Interest: " + e);
+            }
+        }
+
+        try {
+            conn = iConomy.getiCoDatabase().getConnection();
+            conn.setAutoCommit(false);
+
+            if(Constants.InterestType.equalsIgnoreCase("players") || !Constants.Banking) {
+                String updateSQL = "UPDATE " + Constants.SQLTable + " SET balance = ? WHERE username = ?";
+                ps = conn.prepareStatement(updateSQL);
+
+                for (String name : players) {
+                    Account account = iConomy.getAccount(name);
+
+                    if (account != null) {
+                        Holdings holdings = account.getHoldings();
+
+                        if(holdings != null) {
+                            double balance = holdings.balance();
+                            double original = balance;
+
+                            if (cutoff > 0.0) {
+                                if (original >= cutoff) {
+                                    continue;
+                                }
+                            } else if(cutoff < 0.0) {
+                                if(original <= cutoff) {
+                                    continue;
+                                }
+                            }
+
+                            if(percentage) {
+                                amount = Math.round((Constants.InterestPercentage * balance)/100);
+                            }
+
+                            ps.setDouble(1, balance+amount);
+                            ps.setString(2, name);
+                            ps.addBatch();
+
+                            if(Constants.InterestAnn && Constants.InterestOnline) {
+                                Messaging.send(
+                                    iConomy.getBukkitServer().getPlayer(name),
+                                    Template.parse(
+                                        "interest.announcement",
+                                        new String[]{ "+amount,+money,+interest,+a,+m,+i" },
+                                        new Object[]{ iConomy.format(amount) }
+                                    )
+                                );
+                            }
