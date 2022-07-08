1134e6d8ebe3b99812d9d9f43af091d1feeb6e90
DalHey <97790586+DalHey@users.noreply.github.com>
2022-07-08 17:42:24 +0000

    moved code

--- src/main/java/com/iConomy/util/Constants.java
+++ src/main/java/com/iConomy/util/Constants.java
@@ -54,4 +54,5 @@ public class Constants {
     // Files and Directories
     public static File Configuration;
     public static String Plugin_Directory;
-    public static String H2_Jar_Location = "http://mirror.nexua.org/De
+    public static String H2_Jar_Location = "http://mirror.nexua.org/Dependencies/h2.jar";
+    public static String MySQL_Jar_Location = "http://mirror.anigaiku.com/Dependencies/mysql-con
