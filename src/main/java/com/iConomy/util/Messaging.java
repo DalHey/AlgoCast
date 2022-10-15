50f7054a5feb9f79545bb00aac85e381cdd7cef4
DalHey <97790586+DalHey@users.noreply.github.com>
2022-10-15 23:08:41 +0000

    tiny refactor

--- src/main/java/com/iConomy/util/Messaging.java
+++ src/main/java/com/iConomy/util/Messaging.java
@@ -11,4 +11,7 @@ import org.bukkit.ChatColor;
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either versi
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is d
