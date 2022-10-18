f14f5e0e89deec08ff80f044b03700bc93beb50a
DalHey <97790586+DalHey@users.noreply.github.com>
2022-10-18 19:50:04 +0000

    Fix typo in ChangeLog

--- src/main/java/com/iConomy/util/Messaging.java
+++ src/main/java/com/iConomy/util/Messaging.java
@@ -17,4 +17,6 @@ import org.bukkit.ChatColor;
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU 
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public 
