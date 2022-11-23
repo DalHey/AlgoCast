7f5f7351fef6f8537ba846076021b4a2ba024193
DalHey <97790586+DalHey@users.noreply.github.com>
2022-11-23 12:19:50 +0000

    Remove debug message.

--- src/main/java/com/iConomy/util/Messaging.java
+++ src/main/java/com/iConomy/util/Messaging.java
@@ -84,4 +84,5 @@ public class Messaging {
         original = original.replace("`r", ChatColor.RED.toString());
         original = original.replace("`R", ChatColor.DARK_RED.toString());
         original = original.replace("`y", ChatColor.YELLOW.toString());
-        original = original.replace("`Y", ChatColor.GOLD
+        original = original.replace("`Y", ChatColor.GOLD.toString());
+        original = original.replace("`g", ChatColor.GREEN.toString())
