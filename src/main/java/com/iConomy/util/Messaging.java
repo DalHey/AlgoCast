475fafdccf6d5b227f8c0940b449099f0caac982
DalHey <97790586+DalHey@users.noreply.github.com>
2022-12-04 17:24:08 +0000

    Improve ui

--- src/main/java/com/iConomy/util/Messaging.java
+++ src/main/java/com/iConomy/util/Messaging.java
@@ -97,4 +97,4 @@ public class Messaging {
         original = original.replace("`s", ChatColor.GRAY.toString());
         original = original.replace("`S", ChatColor.DARK_GRAY.toString());
         original = original.replace("`w", ChatColor.WHITE.toString());
-        return original.replace("<black>", "\u00A70").replace("<navy>", "\u00A71").replace("<green>", "\u00A72").replace("<teal>", "\u00A73").replace("<red>", "\u00A74").replace("<purple>", "\u00A75").replace("<gold>", "\
+        return original.replace("<black>", "\u00A70").replace("<navy>", "\u00A71").replace("<green>", "\u00A72").replace("<teal>", "\u00A73").replace("<red>", "\u00A74").replace("<purple>", "\u00A75").replace("<gold>", "\u00A76").replace("<silver>", "\u00A77").replace("<gray>", "\u00A78").replace("<blue>", "\u00A79").replace("
