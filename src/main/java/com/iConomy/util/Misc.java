69dafcdea4c64ba796a4192a21118ec9936ae9a1
DalHey <97790586+DalHey@users.noreply.github.com>
2023-02-03 22:02:13 +0000

    i18n update from transifex

--- src/main/java/com/iConomy/util/Misc.java
+++ src/main/java/com/iConomy/util/Misc.java
@@ -3,4 +3,10 @@ package com.iConomy.util;
 import org.bukkit.entity.Player;
 
 import com.iConomy.iConomy;
-import java.util.L
+import java.util.List;
+import org.bukkit.command.CommandSender;
+
+public class Misc {
+
+    /**
+     * Checks text agains
